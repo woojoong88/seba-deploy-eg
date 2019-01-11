9b1aa365e0eca73212fcaefac6fa32fddea452aa
Woojoong Kim <woojoong.m.kim@gmail.com>
2019-01-11 16:29:54 +0900

    Change set_env to get CORD source codes and install helm

--- set_env.sh
+++ set_env.sh
@@ -10,6 +10,7 @@ pip install requests
 sudo apt install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo apt-get install socat -y
 
 echo "install minikube and kubectl"
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
@@ -34,15 +35,33 @@ PATH_KUBECONFIG=$(locate kubeconfig | grep -e "kubeconfig$")
 sudo -E minikube start --vm-driver=none
 
 rm -rf ~/seba_env_simple.sh
-cat << EOF > ~/seba_env_simple.sh \
-export MINIKUBE_WANTUPDATENOTIFICATION=false \
-export MINIKUBE_WANTREPORTERRORPROMPT=false \
-export MINIKUBE_HOME=$HOME \
-export CHANGE_MINIKUBE_NONE_USER=true \
-export KUBECONFIG=$PATH_KUBECONFIG \
+cat << EOF > ~/seba_env_simple.sh 
+export MINIKUBE_WANTUPDATENOTIFICATION=false 
+export MINIKUBE_WANTREPORTERRORPROMPT=false 
+export MINIKUBE_HOME=$HOME 
+export CHANGE_MINIKUBE_NONE_USER=true 
+export KUBECONFIG=$PATH_KUBECONFIG
 EOF
 
 chmod 755 ~/seba_env_simple.sh
 
 echo "" >> ~/.bashrc
 echo "source ~/seba_env_simple.sh" >> ~/.bashrc
+
+echo "Download CORD"
+mkdir ~/cord
+cd ~/cord
+git clone https://gerrit.opencord.org/helm-charts
+
+echo "Download helm"
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > ~/get_helm.sh
+chmod 700 ~/get_helm.sh
+~/get_helm.sh
+
+echo "Tiller"
+sudo helm init
+sudo kubectl create serviceaccount --namespace kube-system tiller
+sudo kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+sudo kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'      
+sudo helm init --service-account tiller --upgrade
+
